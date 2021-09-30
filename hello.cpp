289ba01342e5fc03158ad40d624ff7500deec8f2
Parsa Zakeri <parsa.zakeri@bulme.at>
2021-09-30 12:59:39 +0200

    interactive function added

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,10 @@
 int main()
 {
 	std::cout << "Hallo liebe Leute!" << std::endl;
+	std::cout << "Wie heist du?" << std::endl;
+	std::string Name;
+	std::cin >> Name;
+	std::cout << "Hallo " << Name << std::endl;
+	
 	return 0;
 }
